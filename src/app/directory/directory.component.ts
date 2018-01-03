e5c5d1313c126f7482e7fa2deb458f1e3b5dd89b
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2018-01-03 15:56:38 -0500

    finished lesson 27 on firebase api

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -1,26 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LoggingService } from '../logging.service';
+import { DataService } from '../data.service';
+declare var firebase: any;
 
 @Component({
   selector: 'app-directory',
   templateUrl: './directory.component.html',
-  styleUrls: ['./directory.component.css']
+  styleUrls: ['./directory.component.css'],
+  providers: [DataService]
 })
 export class DirectoryComponent implements OnInit {
-  ninjas = [
-    {name: "Yoshi", belt: 'black'},
-    {name: "Ryu", belt: 'red'},
-    {name: "Crystal", belt: 'purple'}
-  ];
-  constructor(private logger: LoggingService) {  }
+  ninjas = [];
+  constructor(private logger: LoggingService, private dataService: DataService) {  }
 
   logIt() {
     this.logger.log();
   }
 
   ngOnInit() {
+    // this.dataService.fetchData().subscribe(
+    //   (data) => this.ninjas = data;
+    // );
 
+    this.fbGetData();
+  }
+
+  fbGetData() {
+    firebase.database().ref('/').on('child_added', (snapshot) => {
+      this.ninjas.push(snapshot.val())
+    })
   }
 
 }
