fafd6960985f3e45d3a77274d8239f5b42ba60a9
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-01-28 15:45:52 -0500

    working routes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import './polyfills.ts';
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
+
+@NgModule({
+  imports: [
+    HttpModule
+  ]
+})
 
 if (environment.production) {
   enableProdMode();
